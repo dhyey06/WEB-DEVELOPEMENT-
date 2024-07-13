28f6fd86330175aa2604809c8f8b8fab83bb6dbb
dhyey06 <dhyeybhavsar00@gmail.com>
2024-07-13 13:55:50 +0530

    adding button

--- 77/script.js
+++ 77/script.js
@@ -29,4 +29,9 @@ function card(title, cname, views, monthsold, duration, thumbnail){
 document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 }
 
-card("Title", "The Kittler", 560000, 7, "6:06", "https://i.ytimg.com/vi/AkCV9JRuEcg/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCj2Bpa-4u8D7HdSNXbfmAtTn5mfQ")
+
+
+
+function addcard(){
+    card("Title", "The Kittler", 560000, 7, "6:06", "https://i.ytimg.com/vi/AkCV9JRuEcg/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCj2Bpa-4u8D7HdSNXbfmAtTn5mfQ")
+}
